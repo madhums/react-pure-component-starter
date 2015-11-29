240db570a2c12d124cc3c0bf83d3d88f0e68eb23
Madhusudhan Srinivasa <madhums8@gmail.com>
2015-11-29 15:22:41 +0100

    add code comments

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 import history from './lib/history';
 
-// container
+// containers
 import App from './containers/App';
 import Github from './containers/Github';
 
@@ -12,21 +12,28 @@ import About from './components/About';
 import Home from './components/Home';
 import User from './components/User';
 
+// Pure function components
 const foo = () => () => <h3>Foo</h3>;
 const bar = () => () => <h3>Bar</h3>;
-
 const Foo = foo(React);
 const Bar = bar(React);
 
 export default (
   <Router history={history}>
+
+    {/* About routes */}
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="about" component={About} />
+
+      {/* Named components */}
       <Route path="foobar" components={{ goo: Foo, gar: Bar }} />
     </Route>
+
+    {/* Github user route */}
     <Route path="/github" component={Github}>
       <Route path=":username" component={User} />
     </Route>
+
   </Router>
 );
